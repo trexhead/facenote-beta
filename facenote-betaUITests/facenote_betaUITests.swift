763c1811bef11ff99a708b05450fa51cd3a1d175
Terrez Lamont Wells <terrezlamontwells@Terrezs-MacBook-Air.local>
2017-05-09 16:18:43 -0500

    Initial Commit

---
+++ facenote-betaUITests/facenote_betaUITests.swift
@@ -0,0 +1,36 @@
+//
+//  facenote_betaUITests.swift
+//  facenote-betaUITests
+//
+//  Created by Terrez Lamont Wells on 5/9/17.
+//  Copyright © 2017 Terrez Lamont Wells. All rights reserved.
+//
+
+import XCTest
+
+class facenote_betaUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
